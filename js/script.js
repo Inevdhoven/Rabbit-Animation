dd652641c18641902c86c8446d5a209962202e87
Ine <43877754+Inevdhoven@users.noreply.github.com>
2022-12-07 17:35:08 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,11 +1,18 @@
-var buttonSpringen = document.querySelector('section > div div:first-of-type button');
-var buttonEten = document.querySelector('section > div div:nth-of-type(2) button');
-var buttonSlapen = document.querySelector('section > div div:nth-of-type(3) button');
+var buttonStart = document.querySelector('section > div div:first-of-type button');
+var buttonSpringen = document.querySelector('section > div div:nth-of-type(2) button');
+var buttonEten = document.querySelector('section > div div:nth-of-type(3) button');
+var buttonSlapen = document.querySelector('section > div div:nth-of-type(4) button');
 var video = document.querySelector('section > video');
 
+buttonStart.addEventListener('click', spelenIdle);
 buttonSpringen.addEventListener('click', speelSpringen);
 buttonEten.addEventListener('click', speelEten);
 buttonSlapen.addEventListener('click', speelSlapen);
+
+function spelenIdle() {
+    video.play();
+    video.src = "videos/konijn_idle_klaar.mp4";
+}
 
 function speelSpringen() {
     video.src = "videos/konijn_springen_klaar.mp4";
