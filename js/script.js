413503e319bbc9374d64cfe649154db577e5404a
Ine <43877754+Inevdhoven@users.noreply.github.com>
2022-12-07 16:07:12 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -3,24 +3,21 @@ var buttonEten = document.querySelector('section > div div:nth-of-type(2) button
 var buttonSlapen = document.querySelector('section > div div:nth-of-type(3) button');
 var video = document.querySelector('section > video');
 
-video.muted = !video.muted;
+video.muted = false;
 
 buttonSpringen.addEventListener('click', speelSpringen);
 buttonEten.addEventListener('click', speelEten);
 buttonSlapen.addEventListener('click', speelSlapen);
 
 function speelSpringen() {
-    console.log('run springen');
     video.src = "videos/konijn_springen_klaar.mp4";
 }
 
 function speelEten() {
     video.src = "videos/konijn_eten_klaar.mp4";
-    console.log('run eten');
 }
 
 function speelSlapen() {
     video.src = "videos/konijn_slapen_klaar.mp4";
-    console.log('sup slapen');
 }
 
